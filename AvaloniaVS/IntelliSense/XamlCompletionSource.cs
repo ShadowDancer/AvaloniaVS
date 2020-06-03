74d14a4816a99fa0a75d542d16d28a6d94e7c56e
Przemysław Onak <przemekonak@gmail.com>
2020-06-03 23:07:06 +0200

    Pass whole file to CompletionEngine
    This allows completions based on text after cursor

--- AvaloniaVS/IntelliSense/XamlCompletionSource.cs
+++ AvaloniaVS/IntelliSense/XamlCompletionSource.cs
@@ -31,8 +31,9 @@ namespace AvaloniaVS.IntelliSense
                 var sw = Stopwatch.StartNew();
                 var pos = session.TextView.Caret.Position.BufferPosition;
                 var text = pos.Snapshot.GetText(0, pos.Position);
+                var fullText = pos.Snapshot.GetText();
                 _buffer.Properties.TryGetProperty("AssemblyName", out string assemblyName);
-                var completions = _engine.GetCompletions(metadata.CompletionMetadata, text, pos, assemblyName);
+                var completions = _engine.GetCompletions(metadata.CompletionMetadata, text, fullText, pos, assemblyName);
 
                 if (completions?.Completions.Count > 0)
                 {
